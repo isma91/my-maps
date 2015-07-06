18edf43994ed0c1831cf3588344c76485c91772f
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 12:02:27 +0200

    the redirection file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Index.php
+*
+* PHP Version 5.2
+*
+* @category Vue
+* @package  Afichage
+* @author   aydogm_i <ismail.aydogmus@epitech.eu>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     http://localhost:8080/rendu/JavaScript_Avance_my_maps/index.php
+*/
+header("Location: vue/index.php");
+?>
